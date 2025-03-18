fd812f36d929d96344cdeedb7b9889e97c5b3cc3
SolCaruso <caruso@ocelot.ltd>
2025-03-18 12:53:32 -0400

    testing captcha day2-2

--- src/components/contact/ReCaptchaProvider.jsx
+++ src/components/contact/ReCaptchaProvider.jsx
@@ -3,8 +3,12 @@
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 import Script from 'next/script'
 
-export default function ReCaptchaLayout({ children }) {
-  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY;
+export default function ReCaptchaProvider({ children }) {
+  const siteKey = process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
+  if (!siteKey) {
+    console.error('NEXT_PUBLIC_RECAPTCHA_SITE_KEY is not defined')
+    return null
+  }
   return (
     <>
       <Script
