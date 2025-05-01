128e1f5ac2165997ac55913b9366bfad6add4e4d
SolCaruso <caruso@ocelot.ltd>
2025-04-30 20:03:33 -0400

    plays slider videos for mobile as well

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -311,13 +311,21 @@ export default function Slider() {
                 <div className="border border-white/10 rounded-md bg-gradient-to-tr from-[#0C0D0F] to-[#111214] via-[#111214]/75 backdrop-blur-sm shadow-lg w-full max-w-[700px] overflow-hidden">
                   <div className="relative w-full aspect-video">
                     {visitedSlides.has(index) && (
-                      <Image
-                        src={slide.mobile.image}
-                        alt={slide.mobile.headingText}
-                        fill
-                        loading="lazy"
-                        className="object-cover border-b border-white/10"
-                      />
+                      <video
+                        className="w-full h-full object-cover border-b border-white/10"
+                        muted
+                        playsInline
+                        loop
+                        autoPlay
+                        poster={slide.mobile.image}
+                      >
+                        {/* Native HLS for iOS/Safari */}
+                        <source src={slide.desktop.videoSrc} type="application/vnd.apple.mpegURL" />
+                        {/* MP4 fallback */}
+                        {slide.desktop.videoSources.map((source, i) => (
+                          <source key={i} src={source.src} type={source.type} />
+                        ))}
+                      </video>
                     )}
                   </div>
                   <div className="text-white p-5 pt-7">
